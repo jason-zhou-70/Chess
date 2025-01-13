1503824d68026ef7d92adfd6c2d7c06d08b48a5b
Jason Zhou <jasonzhou123@hotmail.com>
2025-01-13 15:39:42 -0800

    add colour choice before game and reset button after game

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
-import { Chess } from 'chess.js'
-import { Chessboard } from 'react-chessboard'
+import ReactModal from 'react-modal'
 import Game from './components/Game'
 import './App.css'
 
 function App() {
+  const [playerColour, setPlayerColour] = useState("white")
+  const [gameStarted, setGameStarted] = useState(false)
 
   return (
     <div>
-      <Game />
+      <ReactModal isOpen={!gameStarted}>
+        <h1>Choose your colour</h1>
+
+        <button onClick={() => {
+          setPlayerColour("white")
+          setGameStarted(true)
+        }}>White</button>
+
+        <button onClick={() => {
+          setPlayerColour("black")
+          setGameStarted(true)
+        }}>Black</button>
+      </ReactModal>
+      {gameStarted && <Game playerColour={playerColour} />}
     </div>
   )
 }
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,30 +1,44 @@
 import { useState, useMemo, useCallback, useEffect } from 'react';
 import { Chess } from 'chess.js';
 import { Chessboard } from 'react-chessboard';
+import ReactModal from 'react-modal';
+import { BoardOrientation } from 'react-chessboard/dist/chessboard/types';
 
-function Game() {
+function Game({playerColour}: {playerColour: string}) {
     const chess: Chess = useMemo(() => new Chess(), []);
     const [fen, setFen] = useState(chess.fen());
+    const [orientation, setOrientation] = useState(playerColour);
+    const [gameOver, setGameOver] = useState("");
 
-    const makeMove = useCallback((move: any) => {
-        try {
-            const result = chess.move(move);
-            setFen(chess.fen());
+    const makeMove = useCallback(
+        // move param has source square, target square, and colour of the piece
+        (move: {
+            from: string;
+            to: string;
+            color: 'w' | 'b';
+        }) => {
+            try {
+                const result = chess.move(move);
+                setFen(chess.fen());
 
-            return result;
-        }
-        catch (e) {
-            return null;
-        }
+                if (chess.isGameOver()) {
+                    setGameOver(chess.isCheckmate() ? "Checkmate!" : "Stalemate!");
+                }
+
+                return result;
+            }
+            catch (e) {
+                return null;
+            }
     }, [chess])
-
+    
     function onDrop(sourceSquare: string, targetSquare: string): boolean {
         const moveData = {
             from: sourceSquare,
             to: targetSquare,
             color: chess.turn()
         };
-
+        
         const move = makeMove(moveData)
 
         if (move === null) {
@@ -35,11 +49,26 @@ function Game() {
     };
 
     return (
+        <>
+            <div>
+                <Chessboard position={fen} onPieceDrop={onDrop} boardWidth={400} boardOrientation={orientation as BoardOrientation}/>
 
-        <div>
-            <Chessboard position={fen} onPieceDrop={onDrop} />
-        </div>
-
+                <ReactModal isOpen={gameOver !== ""}>
+                    <h1>{gameOver}</h1>
+                    <button onClick={() => {
+                        chess.reset();
+                        if (orientation === "white") {
+                            setOrientation("black");
+                        }
+                        else {
+                            setOrientation("white");
+                        }
+                        setFen(chess.fen());
+                        setGameOver("");
+                    }}>Reset</button>
+                </ReactModal>
+            </div>
+        </>
     );
 };
 
