28547b987c44503ee3c1c159821f5f8e5ce469a3
Jason Zhou <jasonzhou123@hotmail.com>
2025-01-16 19:10:54 -0800

    Add minimax algorithm to computer move with basic evaluation heuristic

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -10,6 +10,105 @@ function Game({playerColour}: {playerColour: string}) {
     const [orientation, setOrientation] = useState(playerColour);
     const [gameOver, setGameOver] = useState("");
 
+    // Evaluate the value of the position by checking how many pieces each side has on the board
+    // Rook = 5, Bishop/Knight = 3, Queen = 8, Pawn = 1
+    // Lower-case = black, upper-case = white
+    function evaluate(): number {
+        // Extract the position of the piece from the FEN to count each piece
+        const pos: string = chess.fen().split(' ', 1)[0];
+        let evaluation: number = 0;
+        for (let i = 0; i < pos.length; i++) {
+            switch (pos[i]) {
+                case 'r':
+                    evaluation -= 5;
+                    break;
+                case 'b':
+                case 'n':
+                    evaluation -= 3;
+                    break;
+                case 'q':
+                    evaluation -= 8;
+                    break;
+                case 'p':
+                    evaluation -= 1;
+                    break;
+                case 'R':
+                    evaluation += 5;
+                    break;
+                case 'B':
+                case 'N':
+                    evaluation += 3;
+                    break;
+                case 'Q':
+                    evaluation += 8;
+                    break;
+                case 'P':
+                    evaluation += 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return evaluation;
+    }
+
+    // alpha starts at -Inf, beta starts at Inf
+    function minimax(depth: number, alpha: number, beta: number): number {
+        // Return evaluation if depth is reached
+        if (depth == 0) {
+            return evaluate();
+        }
+        // If position is in checkmate
+        if (chess.isCheckmate()) {
+            // Return small evaluation if white is in mate
+            if (chess.turn() === 'w') {
+                return -Infinity;
+            }
+            // Big evaluation if black is in mate
+            else {
+                return Infinity;
+            }
+        }
+        if (chess.isDraw()) {
+            return 0;
+        }
+        
+        // Maximize if white
+        if (chess.turn() === 'w') {
+            let maxEval: number = -Infinity;
+            const moves: string[] = chess.moves();
+            for (let i = 0; i < moves.length; i++) {
+                // Make the move, evaluate position, undo the move
+                chess.move(moves[i]);
+                let evaluation: number = minimax(depth - 1, alpha, beta);
+                chess.undo();
+                
+                maxEval = Math.max(evaluation, maxEval);
+                alpha = Math.max(maxEval, alpha);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            return maxEval;
+        }
+        // Minimize if black
+        else {
+            let minEval: number = Infinity;
+            const moves: string[] = chess.moves();
+            for (let i = 0; i < moves.length; i++) {
+                chess.move(moves[i]);
+                let evaluation: number = minimax(depth - 1, alpha, beta);
+                chess.undo();
+                minEval = Math.min(evaluation, minEval);
+                beta = Math.min(minEval, beta);
+                if (beta <= alpha) {
+                    break;
+                }
+            }
+            return minEval;
+        }
+    }
+
     const makeMove = useCallback(
         // move param has source square, target square, and promotion if applicable
         (move: string | {
@@ -20,7 +119,6 @@ function Game({playerColour}: {playerColour: string}) {
             try {
                 const result = chess.move(move);
                 setFen(chess.fen());
-
                 if (chess.isGameOver()) {
                     setGameOver(chess.isCheckmate() ? "Checkmate!" : "Stalemate!");
                 }
@@ -33,12 +131,50 @@ function Game({playerColour}: {playerColour: string}) {
     }, [chess])
 
     const makeComputerMove = useCallback(() => {
-        const moves = chess.moves();
+        const moves: string[] = chess.moves();
         if (chess.isGameOver() || moves.length === 0) {
             return;
         }
-        const move = moves[Math.floor(Math.random() * moves.length)];
-        makeMove(move);
+        let bestEval: number;
+        // If computer is white, maximize bestEval
+        if (chess.turn() === 'w') {
+            bestEval = -Infinity;
+            for (let i = 0; i < moves.length; i++) {
+                chess.move(moves[i]);
+                let evaluation = minimax(3, -Infinity, Infinity);
+                chess.undo();
+                // If the eval for current move is lower than the best, remove the move from the list
+                if (evaluation < bestEval) {
+                    moves.splice(i, 1);
+                    i -= 1;
+                }
+                // If its better than the current best, remove all the previous moves checked
+                else if (evaluation > bestEval) {
+                    bestEval = evaluation;
+                    moves.splice(0, i);
+                    i = 0;
+                }
+            }
+        }
+        // If computer is black, minimize bestEval
+        else {
+            bestEval = Infinity
+            for (let i = 0; i < moves.length; i++) {
+                chess.move(moves[i]);
+                let evaluation = minimax(3, -Infinity, Infinity);
+                chess.undo();
+                if (evaluation > bestEval) {
+                    moves.splice(i, 1);
+                    i -= 1;
+                }
+                else if (evaluation < bestEval) {
+                    bestEval = evaluation;
+                    moves.splice(0, i);
+                    i = 0;
+                }
+            }
+        }
+        makeMove(moves[Math.floor(Math.random() * moves.length)]);
     }, [chess]);
     
     function onDrop(sourceSquare: string, targetSquare: string): boolean {
